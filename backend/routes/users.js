f19b6997d254d1af05e508e33a552ce7aa4e8f0e
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-09-28 16:04:17 -0700

    setup backend

---
+++ backend/routes/users.js
@@ -0,0 +1,20 @@
+const router = require('express').Router(); 
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {       //first endpoint , handles get requests
+    User.find()                             // find is mongoose method
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {       // 2nd endpoint, post request
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+//const bodyParser = require('body-parser'); //not needed in new version of express
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config(); // environment variables in .env file
+
+const app = express();                      //create express server
+const port = process.env.PORT || 5000;      //""
+
+app.use(cors());            //middleware
+app.use(express.json());    //parse json, server will send and receive json
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+    );
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter)
+
+
+app.listen(port, () => {    //starts server
+    console.log(`Server is running on port: ${port}`);
+});
