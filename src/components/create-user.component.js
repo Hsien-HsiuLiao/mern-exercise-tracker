536815556ac4d35f242f87a8fedb7248c4e038e7
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-10-03 07:25:35 -0700

    update create user component

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 export default class CreateUsers extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: ''
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+                username: e.target.value
+        });
+    }
+    
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username,
+            
+        }
+
+        console.log(user);
+
+        this.setState({
+            username: ''
+        })
+    }
 
     render() {
         return (
