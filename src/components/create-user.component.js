b4898a8b44ca1d3316ae3457541d54b6cb08017f
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-10-05 16:55:12 -0700

    update create user component

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 //import { Link } from 'react-router-dom';
 
 export default class CreateUsers extends Component {
@@ -29,6 +30,9 @@ export default class CreateUsers extends Component {
         }
 
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
 
         this.setState({
             username: ''
